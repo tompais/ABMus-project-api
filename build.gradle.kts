0c7790ac2abf67ee66d5b66ff2bc13b1abb1a679
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2021-07-22 15:46:19 -0300

    Formatting build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("org.asciidoctor.convert") version "1.5.8"
-	kotlin("jvm") version "1.5.20"
-	kotlin("plugin.spring") version "1.5.20"
-	kotlin("plugin.jpa") version "1.5.20"
+    id("org.springframework.boot") version "2.5.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.asciidoctor.convert") version "1.5.8"
+    kotlin("jvm") version "1.5.20"
+    kotlin("plugin.spring") version "1.5.20"
+    kotlin("plugin.jpa") version "1.5.20"
 }
 
 group = "com.cuaren-coders"
@@ -14,108 +14,108 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-	all {
-		exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+    all {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val snippetsDir by extra { file("build/generated-snippets") }
 val ktlint by configurations.creating
 
 dependencies {
-	// Spring Boot
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-web-services")
-	implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
 
-	// Jackson
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	// Dev Tools
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // Dev Tools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-	// DB Drivers
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("org.postgresql:postgresql")
+    // DB Drivers
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
 
-	// Annotation Processor
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    // Annotation Processor
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-	// Test
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(module = "mockito-core")
-	}
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-	testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.24")
-	testImplementation("io.mockk:mockk:1.12.0")
-	testImplementation("com.ninja-squad:springmockk:3.0.1")
-	testImplementation("io.rest-assured:spring-mock-mvc:4.4.0")
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.24")
+    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    testImplementation("io.rest-assured:spring-mock-mvc:4.4.0")
 
-	// Linter
-	ktlint("com.pinterest:ktlint:0.41.0") {
-		attributes {
-			attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-		}
-	}
+    // Linter
+    ktlint("com.pinterest:ktlint:0.41.0") {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
 }
 
 val outputDir = "${project.buildDir}/reports/ktlint/"
 val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
 
 val ktlintCheck by tasks.creating(JavaExec::class) {
-	inputs.files(inputFiles)
-	outputs.dir(outputDir)
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
 
-	description = "Check Kotlin code style."
-	classpath = ktlint
-	mainClass.set("com.pinterest.ktlint.Main")
-	args = listOf("src/**/*.kt")
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args = listOf("src/**/*.kt")
 }
 
 val ktlintFormat by tasks.creating(JavaExec::class) {
-	inputs.files(inputFiles)
-	outputs.dir(outputDir)
+    inputs.files(inputFiles)
+    outputs.dir(outputDir)
 
-	description = "Fix Kotlin code style deviations."
-	classpath = ktlint
-	mainClass.set("com.pinterest.ktlint.Main")
-	args = listOf("-F", "src/**/*.kt")
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    args = listOf("-F", "src/**/*.kt")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-		allWarningsAsErrors = true
-	}
-	dependsOn(ktlintCheck)
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+        allWarningsAsErrors = true
+    }
+    dependsOn(ktlintCheck)
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.test {
-	outputs.dir(snippetsDir)
+    outputs.dir(snippetsDir)
 }
 
 tasks.asciidoctor {
-	inputs.dir(snippetsDir)
-	dependsOn(tasks.test)
+    inputs.dir(snippetsDir)
+    dependsOn(tasks.test)
 }
